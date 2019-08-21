c045e55e4f6e88743975644c7f9b7ec4d412d020
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-08-21 13:14:59 -0400

    reverted change

--- dist/lightning-spark.js
+++ dist/lightning-spark.js
@@ -11226,7 +11226,7 @@ class ExampleCoreQuadList extends CoreQuadList {
         for (let i = 1; i <= this.length; i++) {
             let str = 'entry ' + i + ': ';
             for (let j = 0; j < 4; j++) {
-                let b = i * 10 + j * 4;
+                let b = i * 20 + j * 4;
                 str += floats[b] + ',' + floats[b+1] + ':' + floats[b+2] + ',' + floats[b+3] + '[' + uints[b+4].toString(16) + '] ';
             }
             lines.push(str);
@@ -12256,7 +12256,7 @@ class WebGLRenderer extends Renderer {
     }
 
     createCoreQuadList(ctx) {
-        return new ExampleCoreQuadList(ctx);
+        return new WebGLCoreQuadList(ctx);
     }
 
     createCoreQuadOperation(ctx, shader, shaderOwner, renderTextureInfo, scissor, index) {
